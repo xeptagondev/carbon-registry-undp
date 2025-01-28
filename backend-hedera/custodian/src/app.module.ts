36c60d1d097316d2d8a3c1f06eba619848105e3c
OshanM <oshanm@xeptagon.com>
2025-01-28 12:58:03 +0530

    - Added typeorm module configs and imports

--- backend-hedera/custodian/src/app.module.ts
+++ backend-hedera/custodian/src/app.module.ts
@@ -1,10 +1,23 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import ormConfig from '@app/core/config/orm-config';
+import { CoreModule } from '@app/core';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+    imports: [
+        ConfigModule.forRoot({ isGlobal: true, envFilePath: '.env' }),
+        TypeOrmModule.forRootAsync({
+            imports: [ConfigModule],
+            inject: [ConfigService],
+            useFactory: (configService: ConfigService) =>
+                ormConfig(configService),
+        }),
+        CoreModule,
+    ],
+    controllers: [AppController],
+    providers: [AppService],
 })
 export class AppModule {}
