cc698823b84c38cee3bd841cdaaf4721b32434cb
OshanM <oshanm@xeptagon.com>
2025-01-30 09:58:59 +0530

    - Added mail templates - Added header constants

---
+++ backend-hedera/custodian/libs/shared/src/mail/constant/mail-header.constant.ts
@@ -0,0 +1,5 @@
+export const USER_REGISTER_HEADER: string =
+    '{{Country Name}} Carbon Registry : Welcome!';
+
+export const ORG_CREATE_HEADER: string =
+    '{{Country Name}} Carbon Registry : Welcome!';
