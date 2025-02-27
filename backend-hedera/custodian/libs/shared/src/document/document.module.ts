1be2d12b4049ee2422ba80cadb6d1a6f90594d70
OshanM <oshanm@xeptagon.com>
2025-02-27 13:10:40 +0530

    - Refactored document service to lib - Fixed bug in assignee org emails

--- backend-hedera/custodian/libs/shared/src/document/document.module.ts
+++ backend-hedera/custodian/libs/shared/src/document/document.module.ts
@@ -1,4 +1,26 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { DocumentEntity } from './entity/document.entity';
+import { ProjectEntity } from '../project/entity/project.entity';
+import { ActivityEntity } from '../activity/entity/activity.entity';
+import { UsersEntity } from '../users/entity/users.entity';
+import { OrganizationEntity } from '../organization/entity/organization.entity';
+import { MailModule } from '../mail/mail.module';
+import { JwtModule } from '@nestjs/jwt';
+import { DocumentService } from './service/document.service';
 
-@Module({})
+@Module({
+    imports: [
+        TypeOrmModule.forFeature([
+            DocumentEntity,
+            ProjectEntity,
+            ActivityEntity,
+            UsersEntity,
+            OrganizationEntity,
+        ]),
+        MailModule,
+        JwtModule,
+    ],
+    providers: [DocumentService],
+})
 export class DocumentModule {}
