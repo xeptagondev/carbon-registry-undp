0ff2592998756f1e068987c2f5a031b0ceda1b14
shaluXept <shalud@xeptagon.com>
2025-02-28 13:47:49 +0530

    refactor: update API paths to use 'ORGANIZATION' terminology for consistency

--- web/src/Components/Investment/AddNewInvestment/investmentCreationComponent.tsx
+++ web/src/Components/Investment/AddNewInvestment/investmentCreationComponent.tsx
@@ -103,7 +103,7 @@ export const InvestmentCreationComponent = (props: any) => {
   const getGovernmentDetails = async () => {
     setLoading(true);
     try {
-      const response = await post(API_PATHS.MINISTRY_LIST, {
+      const response = await post(API_PATHS.ORGANIZATION_DETAILS, {
         page: 1,
         size: 100,
         filterAnd: [
@@ -230,7 +230,7 @@ export const InvestmentCreationComponent = (props: any) => {
 
   const getUserOrganization = async () => {
     try {
-      const response = await post(API_PATHS.MINISTRY_LIST, {
+      const response = await post(API_PATHS.ORGANIZATION_DETAILS, {
         page: 1,
         size: 100,
         filterAnd: [{ key: 'companyId', operation: '=', value: userInfoState?.companyId }],
