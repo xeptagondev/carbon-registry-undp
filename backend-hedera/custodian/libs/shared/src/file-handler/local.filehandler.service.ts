0643ae50a5a23868eaf9fc90c9a6f9293bfcc906
Amila Dissanayake <amilad@xeptagon.com>
2025-03-01 09:19:11 +0530

    upload file location fix

--- backend-hedera/custodian/libs/shared/src/file-handler/local.filehandler.service.ts
+++ backend-hedera/custodian/libs/shared/src/file-handler/local.filehandler.service.ts
@@ -1,27 +1,33 @@
 import { Injectable } from '@nestjs/common';
 import { FileHandlerInterface } from './filehandler.interface';
 import { ConfigService } from '@nestjs/config';
-const fs = require('fs').promises;
-const fsAync = require('fs');
+import { promises as fs } from 'fs';
+import * as fsSync from 'fs';
+import * as path from 'path';
 
 @Injectable()
 export class LocalFileHandlerService implements FileHandlerInterface {
     constructor(private configService: ConfigService) {}
 
-    public async uploadFile(path: string, content: string): Promise<string> {
+    public async uploadFile(
+        filePath: string,
+        content: string,
+    ): Promise<string> {
         const baseUrl = this.configService.get<string>('backendHost');
-        // This must run inside a function marked `async`:
-        const parts = path.split('/');
-        if (parts.length > 1) {
-            const folders = './public/' + parts.slice(0, -1).join('/');
-            if (!(await fsAync.existsSync(folders))) {
-                await fsAync.mkdirSync(folders, { recursive: true });
-            }
+        const fullPath = path.join(process.cwd(), 'public/uploads', filePath);
+
+        const folderPath = path.dirname(fullPath);
+        if (!fsSync.existsSync(folderPath)) {
+            fsSync.mkdirSync(folderPath, { recursive: true });
         }
-        await fs.writeFile('./public/' + path, content, 'base64');
-        return baseUrl + '/' + path;
+
+        await fs.writeFile(fullPath, content, 'base64');
+
+        return `${baseUrl}/uploads/${filePath}`;
     }
-    public getUrl(path: string): Promise<string> {
-        throw new Error('Method not implemented.');
+
+    public async getUrl(filePath: string) {
+        const baseUrl = this.configService.get<string>('backendHost');
+        return `${baseUrl}/uploads/${filePath}`;
     }
 }
