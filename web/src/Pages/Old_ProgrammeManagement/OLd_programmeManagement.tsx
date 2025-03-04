afd43f6a422924879ebbf64eadc8b531c1486c7d
Ravindu Fernando <ravinduf@xeptagon.com>
2025-03-04 16:43:53 +0530

    inf changes

---
+++ web/src/Pages/Old_ProgrammeManagement/OLd_programmeManagement.tsx
@@ -0,0 +1,53 @@
+// not used currently
+// import { useNavigate } from 'react-router-dom';
+// import { useTranslation } from 'react-i18next';
+// import { useAbilityContext } from '../../Casl/Can';
+// import { ProgrammeManagementColumns } from '../../Definitions/Enums/programme.management.columns.enum';
+// import { ProgrammeManagementComponent } from '../../Components/Programme/ProgrammeManagement/programmeManagementComponent';
+// import { ROUTES } from '../../Config/uiRoutingConfig';
+
+// const ProgrammeManagement = () => {
+//   const navigate = useNavigate();
+//   const { t } = useTranslation(['common', 'programme']);
+
+//   const visibleColumns = [
+//     ProgrammeManagementColumns.title,
+//     ProgrammeManagementColumns.company,
+//     ProgrammeManagementColumns.sector,
+//     ProgrammeManagementColumns.currentStage,
+//     ProgrammeManagementColumns.creditBalance,
+//     ProgrammeManagementColumns.creditTransferred,
+//     ProgrammeManagementColumns.certifierId,
+//     ProgrammeManagementColumns.serialNo,
+//     ProgrammeManagementColumns.emissionReductionExpected,
+//     ProgrammeManagementColumns.emissionReductionAchievedandCreditIssued,
+//     ProgrammeManagementColumns.action,
+//   ];
+
+//   const onNavigateToProgrammeView = (record: any) => {
+//     navigate(ROUTES.PROGRAMME_DETAILS_BY_ID(String(record.programmeId)), { state: { record } });
+//   };
+
+//   const onClickAddProgramme = () => {
+//     navigate(ROUTES.ADD_PROGRAMME);
+//   };
+
+//   const onClickAddInvestment = () => {
+//     navigate(ROUTES.ADD_INVESTMENT);
+//   };
+
+//   return (
+//     <ProgrammeManagementComponent
+//       t={t}
+//       visibleColumns={visibleColumns}
+//       onNavigateToProgrammeView={onNavigateToProgrammeView}
+//       onClickAddProgramme={onClickAddProgramme}
+//       enableAddProgramme
+//       useAbilityContext={useAbilityContext}
+//     ></ProgrammeManagementComponent>
+//   );
+// };
+
+// export default ProgrammeManagement;
+
+export {};
