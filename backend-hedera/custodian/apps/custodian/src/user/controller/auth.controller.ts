df4b2e047358a00decf5f0f7350b57b3aa7c1e9c
Amila Dissanayake <amilad@xeptagon.com>
2025-03-09 20:21:37 +0530

    added specific credential api for qa

--- backend-hedera/custodian/apps/custodian/src/user/controller/auth.controller.ts
+++ backend-hedera/custodian/apps/custodian/src/user/controller/auth.controller.ts
@@ -1,14 +1,27 @@
-import { Body, Controller, Post, Put, Query } from '@nestjs/common';
+import {
+    Body,
+    Controller,
+    Get,
+    HttpException,
+    HttpStatus,
+    Post,
+    Put,
+    Query,
+} from '@nestjs/common';
 import { AuthService } from '../service/auth.service';
 import { LoginDto } from '@app/shared/users/dto/login.dto';
 import { RefreshLoginDto } from '@app/shared/users/dto/refresh.login.dto';
 import { RequestTokenDto } from '@app/shared/token/dto/request-token.dto';
 import { PasswordResetDto } from '@app/shared/users/dto/password-reset.dto';
 import { ValidateTokenDto } from '@app/shared/token/dto/validate-token.dto';
+import { ConfigService } from '@nestjs/config';
 
 @Controller('auth')
 export class AuthController {
-    constructor(private readonly authService: AuthService) {}
+    constructor(
+        private readonly authService: AuthService,
+        private readonly configService: ConfigService,
+    ) {}
 
     @Post('login')
     async login(@Body() loginDto: LoginDto): Promise<any> {
@@ -34,5 +47,33 @@ export class AuthController {
             verificationToken: requestId,
         };
         return this.authService.resetPassword(validateToken, passwordReset);
+    }
+
+    @Get('userCredentials')
+    async getUserCredentials(
+        @Query('email') email: string,
+        @Query('qaToken') qaToken: string,
+    ) {
+        if (this.configService.get('APP_ENV') === 'prod') {
+            throw new HttpException(
+                'Not allowed in production',
+                HttpStatus.UNAUTHORIZED,
+            );
+        }
+
+        const validToken = this.configService.get('qaToken');
+        if (qaToken !== validToken) {
+            throw new HttpException(
+                'Invalid QA token',
+                HttpStatus.UNAUTHORIZED,
+            );
+        }
+
+        const password = await this.authService.findPassword(email);
+        if (!password) {
+            throw new HttpException('User not found', HttpStatus.UNAUTHORIZED);
+        }
+
+        return { email: email, password: password };
     }
 }
