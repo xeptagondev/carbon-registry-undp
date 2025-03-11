c79023d65fcd9d22fc12d9d986fd14ab23b950da
Amila Dissanayake <amilad@xeptagon.com>
2025-03-11 18:42:53 +0530

    token issue changes

--- backend-hedera/custodian/libs/shared/src/document/dto/document-action-request.dto.ts
+++ backend-hedera/custodian/libs/shared/src/document/dto/document-action-request.dto.ts
@@ -1,5 +1,6 @@
 import { DocumentStateEnum } from '@app/shared/document/enum/document-state.enum';
 import { ApiProperty } from '@nestjs/swagger';
+import { DocumentEnum } from '../enum/document.enum';
 
 export class DocumentActionDTO {
     @ApiProperty({ type: 'string' })
@@ -7,4 +8,7 @@ export class DocumentActionDTO {
 
     @ApiProperty({ enum: DocumentStateEnum })
     action: DocumentStateEnum;
+
+    @ApiProperty({ enum: DocumentStateEnum })
+    type?: DocumentEnum;
 }
