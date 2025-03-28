3a25a5c46c0205c8a7c0f8f800a1ab2720b56f32
shaluXept <shalud@xeptagon.com>
2025-03-28 08:58:30 +0530

    updated

--- web/src/Components/MonitoringReport/StepperComponent.tsx
+++ web/src/Components/MonitoringReport/StepperComponent.tsx
@@ -145,7 +145,7 @@ const StepperComponent = (props: CustomStepsProps) => {
       // Fetch PDD Data
       const pddResponse = await post(API_PATHS.QUERY_DOCUMENT, {
         refId: state?.documents?.PDD?.refId,
-        DocumentEnum: DocumentEnum.PDD,
+        documentEnum: DocumentEnum.PDD,
       });
       console.log('-----------------PDD Response-----------------', pddResponse);
       if (pddResponse?.statusText === 'SUCCESS') {
@@ -161,7 +161,7 @@ const StepperComponent = (props: CustomStepsProps) => {
       //fetch validation data
       const validationResponse = await post(API_PATHS.QUERY_DOCUMENT, {
         refId: state?.documents?.VALIDATION?.refId,
-        DocumentEnum: DocumentEnum.VALIDATION,
+        documentEnum: DocumentEnum.VALIDATION,
       });
       if (validationResponse?.statusText === 'SUCCESS') {
         validationData = validationResponse?.data?.data;
