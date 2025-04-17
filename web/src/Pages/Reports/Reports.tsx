a5d83c91459ac6bdc80dbb2f4504535c5da4cd80
Ravindu Fernando <ravinduf@xeptagon.com>
2025-04-17 13:08:27 +0530

    added table columns

--- web/src/Pages/Reports/Reports.tsx
+++ web/src/Pages/Reports/Reports.tsx
@@ -2,7 +2,7 @@ import { useTranslation } from "react-i18next";
 import ReportingComponent from "../../Components/Reporting/ReportingComponent";
 
 const Reports = () => {
-  const { i18n } = useTranslation(["common"]);
+  const { i18n } = useTranslation(["common", "reporting"]);
 
   return <ReportingComponent translator={i18n} />
 };
