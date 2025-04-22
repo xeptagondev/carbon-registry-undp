395ff6ecb7bd13560c69e0d0a08e5c7d3e7abb20
Ravindu Fernando <ravinduf@xeptagon.com>
2025-04-22 10:08:15 +0530

    prettier issues fixed

--- web/src/Components/Reporting/ReportingComponent.tsx
+++ web/src/Components/Reporting/ReportingComponent.tsx
@@ -1,14 +1,11 @@
-import { i18n } from "i18next";
-import React, { useState } from "react";
-import "./ReportingComponent.scss";
-import { DatePicker, Empty, Row, Select } from "antd";
-import moment from "moment";
-import ReportCard from "./ReportCard";
-import {
-  getActionsReportColumns,
-  getHoldingsReportColumns,
-} from "./reportingColumns";
-import { REPORT_TYPES } from "./reportTypes";
+import { i18n } from 'i18next';
+import React, { useState } from 'react';
+import './ReportingComponent.scss';
+import { DatePicker, Empty, Row, Select } from 'antd';
+import moment from 'moment';
+import ReportCard from './ReportCard';
+import { getActionsReportColumns, getHoldingsReportColumns } from './reportingColumns';
+import { REPORT_TYPES } from './reportTypes';
 
 const ReportingComponent = (props: { translator: i18n }) => {
   const { translator } = props;
@@ -24,9 +21,7 @@ const ReportingComponent = (props: { translator: i18n }) => {
   });
 
   const [paginationInfo, setPaginationInfo] = useState<
-    Partial<
-      Record<keyof typeof REPORT_TYPES, { page: number; pageSize: number }>
-    >
+    Partial<Record<keyof typeof REPORT_TYPES, { page: number; pageSize: number }>>
   >({
     [REPORT_TYPES.ACTIONS]: {
       page: 0,
@@ -38,11 +33,7 @@ const ReportingComponent = (props: { translator: i18n }) => {
     },
   });
 
-  const handlePaginationInfoChange = (
-    page: number,
-    pageSize: number,
-    reportType: REPORT_TYPES
-  ) => {
+  const handlePaginationInfoChange = (page: number, pageSize: number, reportType: REPORT_TYPES) => {
     setPaginationInfo((prev) => ({
       ...prev,
       [reportType]: {
@@ -51,29 +42,29 @@ const ReportingComponent = (props: { translator: i18n }) => {
       },
     }));
 
-    console.log("-----paginationInfo--------", paginationInfo);
+    console.log('-----paginationInfo--------', paginationInfo);
   };
 
   const [selectedYear, setSelectedYear] = useState<any>();
 
   const checkIfAnyReportIsSelected = () => {
-    let isReportSelected = Object.values(selectedReports).some(Boolean)
-    console.log("---------isReportSelected---------", isReportSelected);
-    return isReportSelected
-  }
+    let isReportSelected = Object.values(selectedReports).some(Boolean);
+    console.log('---------isReportSelected---------', isReportSelected);
+    return isReportSelected;
+  };
 
   return (
     <div className="reporting-container">
       <div className="title-container">
-        <Row justify={"space-between"}>
-          <div className="main">{t("reporting:reportsTitle")}</div>
+        <Row justify={'space-between'}>
+          <div className="main">{t('reporting:reportsTitle')}</div>
         </Row>
-        <Row justify={"end"}>
+        <Row justify={'end'}>
           <DatePicker
             size="large"
             picker="year"
             onChange={(value: any) => {
-              console.log("---------value-----------", value);
+              console.log('---------value-----------', value);
               const date = moment(value).local();
               setSelectedYear(date);
             }}
@@ -84,7 +75,7 @@ const ReportingComponent = (props: { translator: i18n }) => {
           <Select
             size="large"
             placeholder="click to select the Reports to display"
-            mode={"multiple"}
+            mode={'multiple'}
             className="report-type-selector"
             onSelect={(value) =>
               setSelectedReports((prev) => ({
@@ -99,22 +90,18 @@ const ReportingComponent = (props: { translator: i18n }) => {
               }))
             }
           >
-            {Object.keys(REPORT_TYPES).map(
-              (type: keyof typeof REPORT_TYPES) => (
-                <Select.Option value={type}>
-                  {t(`reporting:${type}`)}
-                </Select.Option>
-              )
-            )}
+            {Object.keys(REPORT_TYPES).map((type: keyof typeof REPORT_TYPES) => (
+              <Select.Option value={type}>{t(`reporting:${type}`)}</Select.Option>
+            ))}
           </Select>
         </Row>
       </div>
 
       {selectedReports[REPORT_TYPES.ACTIONS] && (
         <ReportCard
-          title={"Actions Report"}
+          title={'Actions Report'}
           reportType={REPORT_TYPES.ACTIONS}
-          host={"Sri lanka"}
+          host={'Sri lanka'}
           year="2025"
           columns={getActionsReportColumns(t)}
           handlePaginationChange={handlePaginationInfoChange}
@@ -132,9 +119,9 @@ const ReportingComponent = (props: { translator: i18n }) => {
 
       {selectedReports[REPORT_TYPES.HOLDINGS] && (
         <ReportCard
-          title={"Holdings Report"}
+          title={'Holdings Report'}
           reportType={REPORT_TYPES.ACTIONS}
-          host={"Sri lanka"}
+          host={'Sri lanka'}
           year="2025"
           columns={getHoldingsReportColumns(t)}
           handlePaginationChange={handlePaginationInfoChange}
