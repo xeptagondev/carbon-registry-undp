bc662394d4056e1eed906d27b52e5443ed16a62c
Lochana Oshadha <lochanaf@xeptagon.com>
2025-04-28 16:03:21 +0530

    search value trim and order by status fix

--- web/src/Pages/CreditPages/Components/creditTransfersTable.tsx
+++ web/src/Pages/CreditPages/Components/creditTransfersTable.tsx
@@ -60,11 +60,11 @@ export const CreditTransfersTableComponent = (props: any) => {
     const filter: any[] = [];
     const filterOr: any[] = [];
 
-    if (search && search !== '') {
+    if (search && search.trim() !== '') {
       filter.push({
         key: 'project"."title',
         operation: 'like',
-        value: `%${search}%`,
+        value: `%${search.trim()}%`,
       });
     }
 
