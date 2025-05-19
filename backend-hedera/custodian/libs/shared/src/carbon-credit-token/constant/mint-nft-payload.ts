b90eb0695fcdcd32cc7b3e3d808320559ff0ca5d
Amila Dissanayake <amilad@xeptagon.com>
2025-05-19 14:54:33 +0530

    HG-372 Token Issuance (Minting)

--- backend-hedera/custodian/libs/shared/src/carbon-credit-token/constant/mint-nft-payload.ts
+++ backend-hedera/custodian/libs/shared/src/carbon-credit-token/constant/mint-nft-payload.ts
@@ -1,10 +1,7 @@
 export interface MintNFTJobPayload {
     tokenId: string;
     batchSerialNumber: string;
-    metadata: Uint8Array;
     amount: number;
-    accountId: string;
-    privateKey: string;
     projectId: string;
     receiverId: number;
     userId: number;
